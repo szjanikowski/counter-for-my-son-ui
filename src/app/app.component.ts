032b716b097dccf32d02c6cfe6aad61cb9215e6b
Szymon Janikowski <sz.janikowski@gmail.com>
2017-07-03 21:26:02 +0200

    Basic counter logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Rx';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+	startStop = 'START'
+	roundReset = 'RUNDA'
+  hundreds = 0;
+  seconds = 0;
+  minutes = 0;
+  hours = 0;
+  days = 0;
+  ngOnInit(){
+    let timer = Observable.timer(0, 10);
+    timer.subscribe(()=>{ 
+    	this.hundreds += 1;
+    	if(this.hundreds == 100) {
+    		this.seconds += 1;
+    		this.hundreds = 0;
+    	}
+    	if(this.seconds == 60) {
+    		this.minutes += 1
+    		this.seconds = 0;
+    	}
+    	if(this.minutes == 60) {
+    		this.hours += 1
+    		this.minutes = 0;
+    	}
+    	if(this.hours == 24) {
+    		this.days += 1
+    		this.hours = 0;
+    	}
+    });
+  }
 }
